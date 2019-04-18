c73af9579971a1ccef9f787941e15103e1e62549
ferdyandannes <fdannes@gmail.com>
2019-04-18 10:50:49 +0800

    Add files via upload

--- tes.py
+++ tes.py
@@ -48,6 +48,9 @@ ego_baru_y = 0
 obj_baru_x = 0
 obj_baru_y = 0
 
+ego_baru_x2 = 0
+ego_baru_y2 = 0
+
 write_x_ego = 0
 write_y_ego = 0
 
@@ -178,63 +181,26 @@ for i in range(len(testsite_array)):
 
 
 
+    if i % 2 == 0 :
+        ego_baru_x = ego_baru_x2
+        ego_baru_y = ego_baru_y2
+    else :
+        ego_baru_x = ego_baru_x + (-ego_prev_x*1.5)
+        ego_baru_y = ego_baru_y + (-ego_prev_y*1.5)
 
-
-    ### SELF CODE VISUALIZATION ###
-    # Print the position
-    print('ego_X = ', int(320-ego_prev_x), ' ego_Y = ', int(250-ego_prev_y), 
-        ' ego_X_ori = ', -ego_prev_x, ' ego_Y_ori = ', -ego_prev_y)
-    print('obj_X = ', int(320-obj_prev_x), ' obj_Y = ', int(250-obj_prev_y),
-        ' obj_X_ori = ', -obj_prev_x, ' obj_Y_ori = ', -obj_prev_y)
-
-    ego_baru_x = ego_baru_x + (-ego_prev_x*1.5)
-    ego_baru_y = ego_baru_y + (-ego_prev_y*1.5)
-    obj_baru_x = obj_baru_x + (-obj_prev_x*1.5)
-    obj_baru_y = obj_baru_y + (-obj_prev_y*1.5)
-
-    print('ego_X_baru = ', int(ego_baru_x), ' ego_Y_baru = ', int(ego_baru_y))
-    print('obj_X_baru = ', int(obj_baru_x), ' obj_Y_baru = ', int(obj_baru_y))
+        ego_baru_x2 = ego_baru_x2 + (-ego_next_x*1.5)
+        ego_baru_y2 = ego_baru_y2 + (-ego_next_y*1.5)
 
     write_x_ego = 640 - ego_baru_x
     write_y_ego = 500 - ego_baru_y
-    write_x_obj = 640 - obj_baru_x
-    write_y_obj = 500 - obj_baru_y
-    print('write_x_ego = ', int(write_x_ego), ' write_y_ego = ', int(write_y_ego))
-    print(' ')
 
     cv2.circle(traj_new, (int(write_x_ego), int(write_y_ego)) ,1, (0,255,0), 2)
-    cv2.circle(traj_new, (int(write_x_obj), int(write_y_obj)) ,1, (255,0,0), 2)
 
     traj_viz = traj_new.copy()
 
     cv2.rectangle(traj_viz, (int(write_x_ego)-7,int(write_y_ego)-14), (int(write_x_ego)+7,int(write_y_ego)+14), (0,255,0), 2)
-    cv2.rectangle(traj_viz, (int(write_x_obj)-7,int(write_y_obj)-14), (int(write_x_obj)+7,int(write_y_obj)+14), (255,0,0), 2)
 
-    # cv2.imshow("wenakno", traj_new)
-    # cv2.imshow("vizz", traj_viz)
-
-    #### COBA2
-    trajectory = asddsa
-
-    traj_newz = cv2.warpAffine(trajectory, H, (1280, 1000))
-    rotation_window = cv2.getRotationMatrix2D((640, 500), float(ego_prev_rotate2) * 180 / np.pi, 1)
-    new_2 = cv2.warpAffine(traj_newz, rotation_window, (1280, 1000))
-
-    #### ADD CAR OBJECT SHAPE ####
-    cv2.rectangle(new_2, (int(write_x_ego)-7,int(write_y_ego)-14), (int(write_x_ego)+7,int(write_y_ego)+14), (0,0,255), 2)
-    cv2.circle(new_2, (int(write_x_ego), int(write_y_ego)) ,1, (0,0,255), 2)
-
-    cv2.imshow("wenakn2o", new_2)
-
-    ##### END SELF CODE VISUALIZATION #####
-
-    ####### SAVE THE IMAGE FRAME BY FRAME #######
-    #path = '/home/ee401_2/ferdyan_train/struct2depthv2/seq/2'
-    #cv2.imwrite(os.path.join(path , str(i) + '.jpg'), asddsa)
-
-
-    #cv2.circle(traj_new2, (int(write_x_ego), int(write_y_ego)) ,1, (0,255,0), 2)
-    cv2.imshow("wenakno", asddsa)
+    cv2.imshow("wenakno", traj_viz)
     #cv2.imshow("obj", traj_new2)
     cv2.waitKey(1)
     #traj_comb = traj_ego
